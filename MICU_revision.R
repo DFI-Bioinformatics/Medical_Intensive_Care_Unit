7afe73bf7886036823617e4cd7686c130f275059
yingeddi2008 <ying.eddi2008@gmail.com>
2025-02-19 10:45:33 -0800

    revision update

--- MICU_revision.R
+++ MICU_revision.R
@@ -944,52 +944,10 @@ gg_mds <- shannon_mmp_list |>
   xlab("") +
   geom_hline(aes(yintercept = 7.5), linetype = 2, color = "red")
 
-### Enterococcus -----------
 
-library(yingtools2)
-
-entero.gg <- phylo_rel_abd |> 
-  filter(organism == "Enterococcus") |> 
-  left_join(shannon_mmp_list) |> 
-  mutate(thirtyday_mortality_overall = factor(
-    thirtyday_mortality_overall,
-    levels = c("Non-Survivor", "Survivor"))) |> 
-  ggplot(aes(reorder(shotgunSeq_id, Shannon), pctseqs)) +
-  geom_col(color="black", fill = "grey", width = 0.65) +
-  facet_grid(. ~ thirtyday_mortality_overall,
-             scales = "free",
-             space = "free_x"
-  ) +
-  theme_bw() +
-  theme(
-    legend.position = "none",
-    axis.text.x = eb(),
-    axis.ticks.x = eb(),
-    strip.text.x = eb(),
-    strip.background = eb(),
-    axis.title.y = et(color = "black", size = 14),
-    axis.text.y = et(color = "black", size = 12),
-    panel.spacing = unit(0.5, "lines"),
-    panel.grid = eb(),
-    plot.margin = margin(
-      t = 5,
-      r = 5,
-      b = 0,
-      l = 5
-    )
-  ) +
-  scale_y_continuous(
-    expand = expansion(mult = 0.065),
-    breaks = c(0, 0.0012, 0.05, 0.1, 0.5, 1),
-    trans = log_epsilon_trans(0.01)
-  ) +
-  ylab("Enterococcus Abund.\n") +
-  xlab("") +
-  geom_hline(aes(yintercept = 0.0012), linetype = 2, color = "red")
-
-pdf("Results/shannon_mds.entero.tax.pdf", width = 11, height = 6.5)
-gg.stack(gg_metaphlan, gg_shannon, entero.gg, gg_mds, 
-         heights = c(1, 0.45, 0.45, 0.45),
+pdf("Results/shannon_mds.tax.pdf", width = 11, height = 5.5)
+gg.stack(gg_metaphlan, gg_shannon, gg_mds, 
+         heights = c(1, 0.45, 0.45),
          newpage = F)
 dev.off()
 
@@ -1406,388 +1364,25 @@ mds_group.cox.tbl
 
 gt::gtsave(gtsummary::as_gt(mds_group.cox.tbl), file = "Results/msd_grp.test_coxph.png")
 
-# new cut point for Enteros ----------------------------------------------------
 
-library(cutpointr)
-safe_cutpointr <- possibly(.f = cutpointr, otherwise = "Error")
-
-cutpoints_entero <-
-  phylo_rel_abd %>%
-  mutate(thirtyday_mortality_overall_class = if_else(thirtyday_mortality_overall == "Survivor",
-                                                     0, 1)) |> 
-    group_by(organism) |> 
-  group_map(
-    ~ safe_cutpointr(
-      .,
-      pctseqs,
-      thirtyday_mortality_overall_class,
-      method = maximize_metric,
-      metric = youden,
-      pos_class = 0,
-      neg_class = 1,
-      boot_runs = 100,
-      use_midpoints = TRUE,
-      na.rm = T
-    )  |> mutate(organism = unique(pull(.x, organism))),
-    .keep = TRUE
-  ) 
-
-cutpoints_entero_res <- cutpoints_entero |> 
-  map_df(as_tibble) |> 
-  relocate(organism, optimal_cutpoint)
-
-write_csv(cutpoints_entero_res, "Results/cutpoints_entero_res.csv")
-
-# cutpoints_entero_res <- read_csv("Results/cutpoints_entero_res.csv")
-
-## ROC curve with new cutoffs -------------------
-
-phylo_entero <- phylo_rel_abd %>%
-  mutate(thirtyday_mortality_overall_class = if_else(thirtyday_mortality_overall == "Survivor",
-                                                     0, 1)) |> 
-  spread(organism, pctseqs) 
-
-### Enterococcus -----
-pROC_obj_Enterococcus <- pROC::roc(
-  phylo_entero$thirtyday_mortality_overall_class,
-  phylo_entero$Enterococcus,
-  smoothed = TRUE,
-  ci = TRUE,
-  plot = FALSE,
-  auc.polygon = TRUE,
-  best.method = TRUE,
-  print.auc = TRUE,
-  print.auc.col = "black",
-  col = "#2F472F",
-  auc.polygon.border = "black",
-  auc.polygon.col = "gray65",
-  print.thres.best.method = "youden"
-)
-
-coordinates_Enterococcus <-
-  coords(pROC_obj_Enterococcus,
-         "best",
-         ret = c("acc", "threshold", "sens", "spec", "ppv", "npv")
-  )
-
-cairo_pdf(
-  "Results/ROC_curve_cutpoint_Enterococcus_train.pdf",
-  width = 8,
-  height = 6
-)
-
-pROC::roc(
-  phylo_entero$thirtyday_mortality_overall_class,
-  phylo_entero$Enterococcus,
-  smoothed = TRUE,
-  ci = TRUE,
-  plot = TRUE,
-  auc.polygon = TRUE,
-  best.method = TRUE,
-  print.auc = TRUE,
-  print.auc.col = "black",
-  col = "#2F472F",
-  auc.polygon.border = "black",
-  auc.polygon.col = "gray65",
-  print.thres.best.method = "youden"
-)
-
-text(
-  paste("ACC:", round(coordinates_Enterococcus$accuracy, 3) * 100, "%"),
-  x = 0.5,
-  y = 0.45,
-  adj = 0
-)
-text(paste("PPV:", round(coordinates_Enterococcus$ppv, 2)),
-     x = 0.5,
-     y = 0.41,
-     adj = 0
-)
-text(paste("NPV:", round(coordinates_Enterococcus$npv, 2)),
-     x = 0.5,
-     y = 0.37,
-     adj = 0
-)
-text(paste("Threshold:", round(coordinates_Enterococcus$threshold, 4)),
-     x = 0.5,
-     y = 0.33,
-     adj = 0
-)
-
-roc_plot_Enterococcus <- grDevices::recordPlot()
-
-dev.off()
-
-### Enterobacterales -------
-
-pROC_obj_Enterobacterales <- pROC::roc(
-  phylo_entero$thirtyday_mortality_overall_class,
-  phylo_entero$Enterobacterales,
-  smoothed = TRUE,
-  ci = TRUE,
-  plot = FALSE,
-  auc.polygon = TRUE,
-  best.method = TRUE,
-  print.auc = TRUE,
-  print.auc.col = "black",
-  col = "#2F472F",
-  auc.polygon.border = "black",
-  auc.polygon.col = "gray65",
-  print.thres.best.method = "youden"
-)
-
-coordinates_Enterobacterales <-
-  coords(pROC_obj_Enterobacterales,
-         "best",
-         ret = c("acc", "threshold", "sens", "spec", "ppv", "npv")
-  )
-
-cairo_pdf(
-  "Results/ROC_curve_cutpoint_Enterobacterales_train.pdf",
-  width = 8,
-  height = 6
-)
-
-pROC::roc(
-  phylo_entero$thirtyday_mortality_overall_class,
-  phylo_entero$Enterobacterales,
-  smoothed = TRUE,
-  ci = TRUE,
-  plot = TRUE,
-  auc.polygon = TRUE,
-  best.method = TRUE,
-  print.auc = TRUE,
-  print.auc.col = "black",
-  col = "#2F472F",
-  auc.polygon.border = "black",
-  auc.polygon.col = "gray65",
-  print.thres.best.method = "youden"
-)
-
-text(
-  paste("ACC:", round(coordinates_Enterobacterales$accuracy, 3) * 100, "%"),
-  x = 0.5,
-  y = 0.45,
-  adj = 0
-)
-text(paste("PPV:", round(coordinates_Enterobacterales$ppv, 2)),
-     x = 0.5,
-     y = 0.41,
-     adj = 0
-)
-text(paste("NPV:", round(coordinates_Enterobacterales$npv, 2)),
-     x = 0.5,
-     y = 0.37,
-     adj = 0
-)
-text(paste("Threshold:", round(coordinates_Enterobacterales$threshold, 4)),
-     x = 0.5,
-     y = 0.33,
-     adj = 0
-)
-
-roc_plot_Enterococcus <- grDevices::recordPlot()
-
-dev.off()
-
-## see distribution with new cutoffs ----
-
-phylo_cutp <- phylo_rel_abd %>%
-  mutate(thirtyday_mortality_overall_class = if_else(thirtyday_mortality_overall == "Survivor",
-                                                     0, 1)) |> 
-  left_join(cutpoints_entero_res |> 
-              select(organism, optimal_cutpoint, direction)) |> 
-  left_join(cox_df |> 
-              select(shotgunSeq_id, MDS, grouped_md_score))
-
-phylo_cutp |> 
-  mutate(entero_class = if_else(pctseqs <= optimal_cutpoint,
-                                "Low", "High")) |> 
-  dplyr::count(organism, thirtyday_mortality_overall, entero_class) |> 
-  ggplot(aes(thirtyday_mortality_overall, entero_class)) +
-  geom_tile(aes(fill = n)) +
-  facet_grid(. ~ organism) +
-  geom_text(aes(label = n), color = "white", size = 12) +
-  paletteer::scale_fill_paletteer_c("ggthemes::Orange") +
-  theme_bw()
-
-ggsave("Results/cutpoints_entero_res.hm.pdf", height = 4.5, width = 9.5)
-
-phylo_cutp |> 
-  mutate(entero_class = if_else(pctseqs <= optimal_cutpoint,
-                                "Low", "High")) |> 
-  dplyr::count(organism, grouped_md_score, entero_class) |> 
-  ggplot(aes(grouped_md_score, entero_class)) +
-  geom_tile(aes(fill = n)) +
-  facet_grid(. ~ organism) +
-  geom_text(aes(label = n), color = "white", size = 12) +
-  paletteer::scale_fill_paletteer_c("ggthemes::Orange") +
-  theme_bw()
-
-ggsave("Results/cutpoints_entero_res.MDS.hm.pdf", height = 4.5, width = 9.5)
-  
-phylo_rel_abd |> 
-  ggplot(aes(thirtyday_mortality_overall, pctseqs)) +
-  geom_boxplot(outlier.shape = NA) +
-  geom_jitter(alpha = 0.65) +
-  facet_grid(. ~ organism) +
-  geom_hline(data = cutpoints_entero_res,
-             aes(yintercept = optimal_cutpoint),
-             color = "red", linetype = 2 ) +
-  theme_bw()
-
-ggsave("Results/cutpoints_entero_res.pdf", height = 6.5, width = 9.5)
-
-## CoxPH + KM curve with new cutoff -----------------------------------------------------
-
-new_cox_df <- phylo_cutp |> 
-  mutate(entero_class = if_else(pctseqs <= optimal_cutpoint,
-                                paste0("No ",organism, " Domination"),
-                                paste0(organism, " Domination"))) |> 
-  select(shotgunSeq_id, organism, entero_class) |> 
-  spread(organism, entero_class) |> 
-  left_join(cox_df |> 
-              select(shotgunSeq_id,surv_days,
-                     `Charlson Comorbidity Index`,
-                       `SOFA Score`,
-                     thirtyday_mortality_overall_class)) |> 
-  mutate(Enterobacterales = factor(Enterobacterales,
-                                   levels = c("No Enterobacterales Domination","Enterobacterales Domination")),
-         Enterococcus = factor(Enterococcus,
-                                   levels = c("No Enterococcus Domination","Enterococcus Domination")))
-
-### Enterobacterales: as domination or not, dichotomous ---------------
-library(survival)
-
-#### CoxPH ------------------
-new_enteroba <- coxph(
-  Surv(new_cox_df$surv_days, new_cox_df$thirtyday_mortality_overall_class) ~
-    `Charlson Comorbidity Index` +
-    `SOFA Score` +
-    # `Time to stool sample` +  # this will violate the ph assumptions
-    Enterobacterales,
-  data = new_cox_df
-)
-
-new_enteroba.tbl <- new_enteroba %>%
-  gtsummary::tbl_regression(
-    exp = TRUE,
-    pvalue_fun = function(x) {
-      if_else(is.na(x), NA_character_, if_else(
-        x < 0.001,
-        format(x,
-               digits = 3, scientific = TRUE
-        ),
-        format(round(x, 3),
-               scientific = F
-        )
-      ))
-    }
-  ) %>%
-  gtsummary::modify_footnote(everything() ~ NA, abbreviation = TRUE) %>%
-  gtsummary::modify_caption("**Enterobacterales CoxPH**")
-
-new_enteroba.tbl
-
-gt::gtsave(gtsummary::as_gt(new_enteroba.tbl), file = "Results/Enterobacterales_newCP_coxph.png")
-
-#### KM curve -----------------
-surv_object <-
-  Surv(new_cox_df$surv_days, new_cox_df$thirtyday_mortality_overall_class)
-
-fit_Enterobac <- survfit(surv_object ~ Enterobacterales, data = new_cox_df)
-
-ggs_Enterobac <- ggsurvplot(
-  fit_Enterobac,
-  data = new_cox_df,
-  size = 1,
-  palette = c( "#107b1c","#C45258"),
-  xlab = "Days from Admission",
-  conf.int = TRUE,
-  pval = TRUE,
-  risk.table = "abs_pct",
-  legend = "bottom",
-  
-  risk.table.height = 0.45,
-  risk.table.y.text.col = TRUE,
-  risk.table.fontsize = 3.25,
-  pval.size = 3.5,
-  tables.y.text = FALSE,
-  ggtheme = theme_test() + theme(
-    panel.grid.major = el(linewidth = 0.25, color = "gray90"),
-    axis.text.y = et(color = "black", size = 8),
-    axis.title.y = et(color = "black")
-  ),
-  legend.labs = c("No Domination", "Enterobacterales Domination")
-)
-
-# Change table axis labels
-ggs_Enterobac$table <-
-  ggs_Enterobac$table + labs(x = NULL, y = NULL) + theme(plot.title = eb()) # risk table
-
-ggs_Enterobac
-
-pdf(
-  "./Results/kaplan_meier_train_newEnterobac.pdf",
-  height = 4,
-  width = 6,
-  onefile = FALSE
-)
-ggs_Enterobac
-invisible(dev.off())
-
-### Enterobacterales: as continuous variable ---------------
+## CoxPH for Enterococcus: as continuous variable ---------------
 
 entero_cox_df <- phylo_rel_abd |> 
   select(shotgunSeq_id, organism, pctseqs) |> 
   spread(organism, pctseqs) |> 
-  left_join(new_cox_df |> 
-              select(-c(Enterobacterales, Enterococcus)))
+  left_join(cox_df)
 
 #### CoxPH -------------
-new_enteroba <- coxph(
+Enterococcus_cont <- coxph(
   Surv(entero_cox_df$surv_days, entero_cox_df$thirtyday_mortality_overall_class) ~
     `Charlson Comorbidity Index` +
     `SOFA Score` +
     # `Time to stool sample` +  # this will violate the ph assumptions
-    Enterobacterales,
+    Enterococcus,
   data = entero_cox_df
 )
 
-new_enteroba.tbl <- new_enteroba %>%
-  gtsummary::tbl_regression(
-    exp = TRUE,
-    pvalue_fun = function(x) {
-      if_else(is.na(x), NA_character_, if_else(
-        x < 0.001,
-        format(x,
-               digits = 3, scientific = TRUE
-        ),
-        format(round(x, 3),
-               scientific = F
-        )
-      ))
-    }
-  ) %>%
-  gtsummary::modify_footnote(everything() ~ NA, abbreviation = TRUE) %>%
-  gtsummary::modify_caption("**Enterobacterales CoxPH**")
-
-new_enteroba.tbl
-
-gt::gtsave(gtsummary::as_gt(new_enteroba.tbl), file = "Results/Enterobacterales_newCP_cont.coxph.png")
-
-### Enterococcus: as domination or not, dichotomous  ------------------------
-
-#### CoxPH ------------
-new_enterocc <- coxph(
-  Surv(new_cox_df$surv_days, new_cox_df$thirtyday_mortality_overall_class) ~
-    `Charlson Comorbidity Index` +
-    `SOFA Score` +
-    Enterococcus,
-  data = new_cox_df
-)
-
-new_enterocc.tbl <- new_enterocc %>%
+Enterococcus_cont.tbl <- Enterococcus_cont %>%
   gtsummary::tbl_regression(
     exp = TRUE,
     pvalue_fun = function(x) {
@@ -1805,92 +1400,13 @@ new_enterocc.tbl <- new_enterocc %>%
   gtsummary::modify_footnote(everything() ~ NA, abbreviation = TRUE) %>%
   gtsummary::modify_caption("**Enterococcus CoxPH**")
 
-new_enterocc.tbl
+Enterococcus_cont.tbl
 
-gt::gtsave(gtsummary::as_gt(new_enterocc.tbl), file = "Results/Enterococcus_newCP_coxph.png")
-
-grid.draw.ggsurvplot <- function(x){
-  survminer:::print.ggsurvplot(x, newpage = FALSE)
-}
-
-#### KM curve ---------------
-fit_Enteroco <- survfit(surv_object ~ Enterococcus, data = new_cox_df)
-
-ggs_Enteroco <- ggsurvplot(
-  fit_Enteroco,
-  data = new_cox_df,
-  size = 1,
-  palette = c( "#107b1c","#C45258"),
-  xlab = "Days from Admission",
-  conf.int = TRUE,
-  pval = TRUE,
-  risk.table = "abs_pct",
-  legend = "bottom",
-  tables.y.text = FALSE,
-  risk.table.height = 0.45,
-  risk.table.y.text.col = TRUE,
-  risk.table.fontsize = 3.25,
-  pval.size = 3.5,
-  ggtheme = theme_test() + theme(
-    panel.grid.major = el(linewidth = 0.25, color = "gray90"),
-    axis.text.y = et(color = "black", size = 8),
-    axis.title.y = et(color = "black")
-  ),
-  legend.labs = c("No Domination", "Enterococcus Domination")
-)
-
-# Change table axis labels
-ggs_Enteroco$table <-
-  ggs_Enteroco$table + 
-  labs(x = NULL, y = NULL) + 
-  theme(plot.title = eb()) # risk table
-
-ggs_Enteroco
-
-ggsave(
-  "./Results/kaplan_meier_train_newEnteroco.pdf",
-  ggs_Enteroco,
-  height = 4,
-  width = 6,
-)
-
-### Enterococcus: as continuous variable ------
-
-#### CoxPH ------------------
-new_enterocc <- coxph(
-  Surv(entero_cox_df$surv_days, entero_cox_df$thirtyday_mortality_overall_class) ~
-    `Charlson Comorbidity Index` +
-    `SOFA Score` +
-    Enterococcus,
-  data = entero_cox_df
-)
-
-new_enterocc.tbl <- new_enterocc %>%
-  gtsummary::tbl_regression(
-    exp = TRUE,
-    pvalue_fun = function(x) {
-      if_else(is.na(x), NA_character_, if_else(
-        x < 0.001,
-        format(x,
-               digits = 3, scientific = TRUE
-        ),
-        format(round(x, 3),
-               scientific = F
-        )
-      ))
-    }
-  ) %>%
-  gtsummary::modify_footnote(everything() ~ NA, abbreviation = TRUE) %>%
-  gtsummary::modify_caption("**Enterococcus CoxPH**")
-
-new_enterocc.tbl
-
-gt::gtsave(gtsummary::as_gt(new_enterocc.tbl), file = "Results/Enterococcus_newCP_cont.coxph.png")
-
+gt::gtsave(gtsummary::as_gt(Enterococcus_cont.tbl), file = "Results/Enterococcus_cont.coxph.png")
 
 #### KM curve for MDS + enterococcus: training ------------------------------------
 
-alt_cox_df <- new_cox_df |> 
+alt_cox_df <- entero_cox_df |> 
   left_join(cox_df |> select(shotgunSeq_id, grouped_md_score)) |> 
   mutate(MDS_entero = if_else(
     grouped_md_score == "High Score" | Enterococcus == "Enterococcus Domination",
@@ -1992,61 +1508,29 @@ ggsave(
   width = 6,
 )
 
-### Enteroccus vs mds ------------------------------------------------
-
-entero_pct_mds <- cox_df |> 
-  left_join(phylo_cutp |> 
-              filter(organism == "Enterococcus") |> 
-              select(shotgunSeq_id, pctseqs, organism))
-
-cor.test(entero_pct_mds$MDS,
-         entero_pct_mds$pctseqs,
-         method = "spearman")
-# rho = 0.3136491
-
-library(ggpmisc)
-
-entero_pct_mds |> 
-  ggplot(aes(MDS, pctseqs)) +
-  geom_point(alpha = 0.65) +
-  stat_poly_line() +
-  # stat_poly_eq(label.x = 1, use_label(c("eq", "R2", "p"))) +
-  stat_poly_eq(label.x = 1, use_label(c("R2", "p"))) +
-  theme_bw() +
-  labs(x="MDS", y = "Enterococcus abundance")
-
-ggsave("Results/enteroc_mds.liner.pdf", height = 6.5, width = 7.5)
-
 # cox.zph assumption check ------------------------------------------------
 
 library(survminer)
 library(survival)
 library(gridExtra)
 
-cox_all <- cox_df |> 
-  select(-c(`Enterococcus Domination`,`Enterobacterales Domination`)) |> 
+cox_all <- entero_cox_df |> 
   left_join(mmp_df |> 
-              select(metabolomicsID, mmp_score)) |> 
-  left_join(new_cox_df |> 
-              select(shotgunSeq_id,
-                     `Enterococcus Domination` = Enterococcus,
-                     `Enterobacterales Domination` = Enterobacterales)) |> 
-  left_join(phylo_entero |>
-              select(shotgunSeq_id, Enterobacterales, Enterococcus))
+              select(metabolomicsID, mmp_score, grouped_mmp_score)) 
 
 base.chosen.cox <- coxph(
   Surv(cox_all$surv_days, cox_all$thirtyday_mortality_overall_class) ~
     `Charlson Comorbidity Index` +
     `SOFA Score` +
     # `Time to stool sample` +  # this will violate the ph assumptions
-    `Shannon Diversity` +
-    shannon_class +
+    `MDS` +
+    grouped_md_score +
     Enterococcus +
     `Enterococcus Domination` +
-    Enterobacterales +
-    `Enterobacterales Domination` +
+    `Shannon Diversity` +
+    shannon_class +
     mmp_score +
-    `MDS`
+    grouped_mmp_score 
   ,
   data = cox_all
 )
